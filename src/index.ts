f949dad9a19b53c8b22a7b7419702a09df4de6d4
Wyatt Barnes <me@wyatt.email>
2022-10-03 15:46:09 -1000

    Remove license headers

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
-/*
-This file is part of web3.js.
-
-web3.js is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-web3.js is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
-*/
 import { ContractAbi } from 'web3-eth-abi';
 import Contract from 'web3-eth-contract';
 import { Web3Context, Web3PluginBase } from 'web3-core';
 import { Address, Web3APISpec } from 'web3-types';
 // @ts-expect-error 'Web3' is declared but its value is never read.
-import { Web3 } from 'web3';
+import Web3 from 'web3';
 
 import { AggregatorV3InterfaceABI } from './aggregator_v3_interface_abi';
 import { Price } from './types';
