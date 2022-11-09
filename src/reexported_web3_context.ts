6c6d934dccd96299b9555f3f7059b5bdc54e4933
Wyatt Barnes <me@wyatt.email>
2022-11-09 11:07:23 -1000

    Rename reexported_web3_objects.ts to reexported_web3_context.ts and remove Web3 and Web3Eth re-exports

---
+++ src/reexported_web3_context.ts
@@ -0,0 +1,3 @@
+import { Web3Context } from 'web3-core';
+
+export { Web3Context };
--- src/reexported_web3_objects.ts
+++
@@ -1,5 +0,0 @@
-import Web3 from 'web3';
-import { Web3Context } from 'web3-core';
-import Web3Eth from 'web3-eth';
-
-export { Web3, Web3Context, Web3Eth };
