c00bc241d223b2a33952508460a4bfdfa38edf91
Wyatt Barnes <me@wyatt.email>
2022-11-17 14:23:38 -1000

    Remove commands.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -13,12 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
 import 'cypress-jest-adapter';
 
 // Enable the hook to match Jest
