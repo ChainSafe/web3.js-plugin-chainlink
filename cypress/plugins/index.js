e50932a4aad9228ee1764da4bda99306aba0da0f
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-06-04 22:20:03 +0200

    add empty space to trigger github actions

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -21,5 +21,6 @@ const webpackOptions = require('../webpack.config.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
 	on('file:preprocessor', webpackPreprocessor({ webpackOptions }));
+
 	return config;
 };
