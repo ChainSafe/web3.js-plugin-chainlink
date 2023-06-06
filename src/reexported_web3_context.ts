f6b5200ef1b0c73451b75032eed23ec8da03088d
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-06-06 14:29:12 +0200

    try a workaround for the module augmentation

--- src/reexported_web3_context.ts
+++ src/reexported_web3_context.ts
@@ -1,4 +1,7 @@
-import { core } from 'web3';
+// import { core } from 'web3';
 
-const { Web3Context } = core;
+// const { Web3Context } = core;
+
+import { Web3Context } from 'web3-core';
+
 export { Web3Context };
